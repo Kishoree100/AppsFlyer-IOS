31cfe126f1b1ef6792716dea91aca9870dd12562
Kishoree <kishoree100@gmail.com>
2025-02-07 11:45:09 +0530

    Initial Commit

---
+++ Test_IOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test_IOS
+//
+//  Created by Kishoree Londhe on 07/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Test_IOSTests/Test_IOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  Test_IOSTests.swift
+//  Test_IOSTests
+//
+//  Created by Kishoree Londhe on 07/02/25.
+//
+
+import Testing
+@testable import Test_IOS
+
+struct Test_IOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
